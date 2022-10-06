d1cf47f64f62f3554e6212236a2a2b83cd50197f
Javoso <lucasqueirozqxd@gmail.com>
2022-10-06 02:12:48 -0300

    Conclusão do módulo 4 do segundo curso da formação de angular da alura.

--- 2- PROJETO_DO_SEGUNDO_CURSO_DA_FORMACAO/angular_formularios-main/gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
+++ 2- PROJETO_DO_SEGUNDO_CURSO_DA_FORMACAO/angular_formularios-main/gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { NovoUsuarioService } from './novo-usuario.service';
 import { NovoUsuario } from './novo-usuario';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { minusculoValidator } from './minusculo.validator';
+import { UsuarioExisteService } from './usuario-existe.service';
+import { usuarioSenhaIguaisValidator } from './usuario-senha-iguais.validator';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-novo-usuario',
@@ -10,23 +14,34 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 })
 export class NovoUsuarioComponent implements OnInit {
 
-  constructor(private novoUsuarioService: NovoUsuarioService, private formBuilder: FormBuilder) { }
+  constructor(
+    private usuarioExistente: UsuarioExisteService,
+    private novoUsuarioService: NovoUsuarioService,
+    private formBuilder: FormBuilder,
+    private router: Router) { }
 
   novoUsuarioForm!: FormGroup;
 
   ngOnInit(): void {
     this.novoUsuarioForm = this.formBuilder.group({
-      email: [''],
-      fullName: [''],
-      userName:[''],
+      email: ['',[Validators.required, Validators.email]],
+      fullName: ['',[Validators.required, Validators.minLength(4)]],
+      userName:['',[minusculoValidator], [this.usuarioExistente.usuarioJaExiste()]],
       password:[''],
+    },
+    {
+      validators: [usuarioSenhaIguaisValidator],
     });
   }
 
   cadastrarUmNovoUsuario(){
-    const novoUser = this.novoUsuarioForm.getRawValue() as NovoUsuario;
-    console.log(novoUser);
-
-    //this.novoUsuarioService.cadastraUmNovoUsuario(this.novoUsuarioForm.getRawValue() as NovoUsuario);
+    if(this.novoUsuarioForm.valid){
+      const novoUser = this.novoUsuarioForm.getRawValue() as NovoUsuario;
+      this.novoUsuarioService.cadastraUmNovoUsuario(novoUser).subscribe(()=>{
+        this.router.navigate(['']);
+      },(error)=>{
+        console.log(error);
+      });
+    }
   }
 }
--- 2- PROJETO_DO_SEGUNDO_CURSO_DA_FORMACAO/angular_formularios-main/gatitobook/src/app/home/novo-usuario/novo-usuario.service.ts
+++ 2- PROJETO_DO_SEGUNDO_CURSO_DA_FORMACAO/angular_formularios-main/gatitobook/src/app/home/novo-usuario/novo-usuario.service.ts
@@ -10,7 +10,10 @@ export class NovoUsuarioService {
   constructor(private httpClient: HttpClient) { }
 
   cadastraUmNovoUsuario(novoUsuario: NovoUsuario) {
-    this.httpClient.post('http://localhost:3000/user/signup', novoUsuario);
+    return this.httpClient.post('http://localhost:3000/user/signup', novoUsuario);
+  }
 
+  verificarUsuarioExistente(nomeUsuario:string){
+    return this.httpClient.get(`http://localhost:3000/user/exists/${nomeUsuario}`);
   }
 }
