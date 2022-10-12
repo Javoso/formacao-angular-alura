5df0ee88f3c1bab4f26cbfb8cbef567480a70591
Javoso <lucasqueirozqxd@gmail.com>
2022-10-12 01:41:30 -0300

    Conclusão do quinto e último módulo do quinto curso da formação de angular da alura.

--- 5- PROJETO_DO_QUINTO_CURSO_DA_FORMACAO/angular-introducao-componentes-acessibilidade-parte2/src/app/app.component.ts
+++ 5- PROJETO_DO_QUINTO_CURSO_DA_FORMACAO/angular-introducao-componentes-acessibilidade-parte2/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, ViewChild, TemplateRef } from '@angular/core';
+import { Component, ViewChild, TemplateRef, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ModalRef } from './shared/components/modal/models/model-ref';
 import { ModalService } from './shared/components/modal/services/modal.service';
 
@@ -7,22 +8,40 @@ import { ModalService } from './shared/components/modal/services/modal.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   @ViewChild('modal') public modalTemplateRef: TemplateRef<any>;
-  title = 'a11y-p2';
+  title ='Angular - Accessibility';
 
   public firstName: string = 'Lucas';
   public surName: string = 'Nascimento';
   public age: number = 25;
   public modalRef: ModalRef;
   public info: boolean = false;
+  public form: FormGroup;
 
-  constructor(private modalService: ModalService) {}
+  constructor(private modalService: ModalService, private formBuilder: FormBuilder) {}
+  
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      firstName : ['', Validators.required],
+      surName : ['', Validators.required],
+      age : ['', Validators.required], 
+      info : [false]
+    });
+  }
 
   public show(): void {
     this.modalRef = this.modalService.open({
       templateRef: this.modalTemplateRef,
       title: 'User Details',
     });
+  }
+
+  public submit():void {
+    if(this.form.invalid){
+      return;
+    }
+    console.log(this.form.value);
+    this.modalRef.close();
   }
 }
